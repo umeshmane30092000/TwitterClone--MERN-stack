b8c07b5468377bd163fd775cadf85e015aa0acf2
Umesh <umeshmane575@gmail.com>
2024-01-23 18:10:39 +0530

    react app is created

---
+++ frontend/src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter , Routes , Route } from "react-router-dom"
+import Home from './pages/Home/Home';
+import Login from './pages/Login/Login';
+
+
+function App() {
+  return (
+    <div className="App">
+
+    <BrowserRouter>
+      <Routes>
+       <Route path = '/' element={<Home/>} />
+       <Route path = '/login' element = {<Login/>} />
+      </Routes>
+    </BrowserRouter>
+      
+    </div>
+  );
+}
+
+export default App;
