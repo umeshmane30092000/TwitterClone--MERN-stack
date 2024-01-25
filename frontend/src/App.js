6072e62eec22885a7855e2e53720a186867e1494
Umesh <umeshmane575@gmail.com>
2024-01-25 10:36:42 +0530

    style is added to both login and signup page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { BrowserRouter , Routes , Route } from "react-router-dom"
 import Home from './pages/Home/Home';
 import Login from './pages/Login/Login';
+import Signup from './pages/Login/Signup';
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <Routes>
        <Route path = '/' element={<Home/>} />
        <Route path = '/login' element = {<Login/>} />
+       <Route path = '/signup' element = {<Signup/>} />
       </Routes>
     </BrowserRouter>
       
