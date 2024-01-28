fd3d663285b9fbbb957d307dd3db662521877218
Umesh <umeshmane575@gmail.com>
2024-01-28 10:53:14 +0530

    we have connected db to frontend uisng featch

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,62 @@
 const express = require('express')
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000;
+require('dotenv').config();
 
 app.use(cors());
 app.use(express.json());
 
 
+const uri = process.env.MONGODB_URI;
+const client = new MongoClient(uri, { serverApi: { version: ServerApiVersion.v1, strict: true, deprecationErrors: true } });
+
+// async function run() {
+//   try {
+//     await client.connect();
+//     await client.db("admin").command({ ping: 1 });
+//     console.log("Successfully connected to MongoDB!");
+//   } finally {
+//     await client.close();
+//   }
+// }
+// run().catch(console.dir);
+
+async function run() {
+    try {
+        await client.connect();
+        console.log("Connected to MongoDB!"); // Added line
+        const postCollection = client.db("database").collection("posts"); // this collection is for team-ekt
+        const userCollection = client.db("database").collection("users"); // this collection is for team-srv
+
+       
+        app.get('/post', async (req, res) => {
+            const post = (await postCollection.find().toArray())
+            res.send(post);
+        })
+        
+        app.post('/post', async (req, res) => {
+            const post = req.body;
+            const result = await postCollection.insertOne(post);
+            res.send(result);
+        })
+
+       
+
+
+
+        
+    } catch (error) {
+        console.log(error);
+    }
+} run().catch(console.dir);
+
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send('Hello from Twitter Clone!')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Twitter clone is listening on port ${port}`)
 })
--- frontend/src/pages/Feed/TweetBox/TweetBox.js
+++ frontend/src/pages/Feed/TweetBox/TweetBox.js
@@ -9,38 +9,58 @@ const TweetBox = () => {
 
     const handleTweet = (e) => {
         e.preventDefault();
-        console.log(post);
+        const userPost = {
+            post: post,
+            photo: imageURL,
+        }
+        console.log(userPost);
 
-    }
+        // comment
+        fetch('http://localhost:5000/post', {
+            method: "POST",
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(userPost),
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
 
-    return (
-        <div className="tweetBox">
-            <form onSubmit={handleTweet}>
-                <div className="tweetBox__input">
-                    <Avatar src="https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.pn" />
-                    <input
-                        type="text"
-                        placeholder="What's happening?"
-                        onChange={(e) => setPost(e.target.value)}
+            })
+        
+    //comment
 
-                    />
-                </div>
+}
 
-                <div className="imageIcon_tweetButton">
+return (
+    <div className="tweetBox">
+        <form onSubmit={handleTweet}>
+            <div className="tweetBox__input">
+                <Avatar src="https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.pn" />
+                <input
+                    type="text"
+                    placeholder="What's happening?"
+                    onChange={(e) => setPost(e.target.value)}
+
+                />
+            </div>
+
+            <div className="imageIcon_tweetButton">
                 <label htmlFor='image' className="imageIcon">
-                <AddPhotoAlternateOutlinedIcon/>
+                    <AddPhotoAlternateOutlinedIcon />
                 </label>
                 <input
                     type="file"
                     id='image'
                     className="imageInput"
-                   
+
                 />
-                 <Button className="tweetBox__tweetButton" type="submit">Tweet</Button>
-                </div>
-            </form>
-        </div>
-    )
+                <Button className="tweetBox__tweetButton" type="submit">Tweet</Button>
+            </div>
+        </form>
+    </div>
+)
 }
 
 export default TweetBox
