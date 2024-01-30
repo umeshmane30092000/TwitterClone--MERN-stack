db25864cd20fa3638c74356c91af015d0afb7055
Umesh <umeshmane575@gmail.com>
2024-01-30 16:27:06 +0530

    All required routes is created such as userpost

--- backend/index.js
+++ backend/index.js
@@ -30,27 +30,24 @@ async function run() {
         const postCollection = client.db("database").collection("posts"); // this collection is for team-ekt
         const userCollection = client.db("database").collection("users"); // this collection is for team-srv
 
-
-        app.get('/post', async (req, res) => {
-            const post = (await postCollection.find().toArray()).reverse();
-            res.send(post);
-        })
         // get
         app.get('/user', async (req, res) => {
             const user = await userCollection.find().toArray();
             res.send(user);
         })
-
         app.get('/loggedInUser', async (req, res) => {
             const email = req.query.email;
             const user = await userCollection.find({ email: email }).toArray();
             res.send(user);
         })
-
-        app.post('/post', async (req, res) => {
-            const post = req.body;
-            const result = await postCollection.insertOne(post);
-            res.send(result);
+        app.get('/post', async (req, res) => {
+            const post = (await postCollection.find().toArray()).reverse();
+            res.send(post);
+        })
+        app.get('/userPost', async (req, res) => {
+            const email = req.query.email;
+            const post = (await postCollection.find({ email: email }).toArray()).reverse();
+            res.send(post);
         })
 
         // post
@@ -60,9 +57,21 @@ async function run() {
             res.send(result);
         })
 
+        app.post('/post', async (req, res) => {
+            const post = req.body;
+            const result = await postCollection.insertOne(post);
+            res.send(result);
+        })
 
-
-
+        // patch
+        app.patch('/userUpdates/:email', async (req, res) => {
+            const filter = req.params;
+            const profile = req.body;
+            const options = { upsert: true };
+            const updateDoc = { $set: profile };
+            const result = await userCollection.updateOne(filter, updateDoc, options);
+            res.send(result)
+        })
 
 
     } catch (error) {
